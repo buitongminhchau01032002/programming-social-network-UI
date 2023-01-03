4302697788405ded613bc04f9a9c93f0e64bbc7a
Minh Chau <buitongminhchau01032002@gmail.com>
2023-01-03 22:22:48 +0700

    header user menu

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,10 +1,14 @@
+import { Popover } from '@headlessui/react';
+
 const USER = {
     displayName: 'Nguyễn Văn A',
     avatar: 'https://picsum.photos/100/100',
+    email: 'emailgifddos@gmail.com',
+    role: 'admin',
 };
 
 function Header() {
-    const user = false;
+    const user = true;
     return (
         <header className="fixed flex h-14 w-full items-center justify-between border-b bg-white px-16">
             <div>LOGO</div>
@@ -39,20 +43,39 @@ function Header() {
                         <button className="h-9 w-9 overflow-hidden rounded-full ring-primary hover:ring-2">
                             <img className="h-full w-full object-contain" src={USER.avatar} />
                         </button>
-                        <button className="hover:text-primary-dark">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="h-4 w-4"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </button>
+
+                        <Popover className="relative">
+                            <Popover.Button as="button" className="outline-none hover:text-primary-dark">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="h-4 w-4"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </Popover.Button>
+
+                            <Popover.Panel className="absolute top-full right-0 z-10 w-80 translate-y-3 space-y-3 rounded-lg border bg-white p-3 shadow-xl">
+                                <button className="flex w-full items-center border-b pb-2">
+                                    <div className="w-1h-11 h-11 overflow-hidden rounded-full">
+                                        <img className="h-full w-full object-contain" src={USER.avatar} />
+                                    </div>
+                                    <div className="ml-3 flex-1">
+                                        <p className="text-left font-bold">{USER.displayName}</p>
+                                        <p className="text-left text-sm text-gray-600">{USER.email}</p>
+                                    </div>
+                                </button>
+
+                                <button className="flex h-9 w-full min-w-[120px] items-center justify-center rounded-md bg-primary px-5 text-sm font-medium text-white transition hover:bg-primary-dark">
+                                    Đăng xuất
+                                </button>
+                            </Popover.Panel>
+                        </Popover>
                     </div>
                 </div>
             ) : (
