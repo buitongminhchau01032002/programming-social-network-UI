70ee98468087eda7233af02e4165feee4b7e7cc3
Minh Chau <buitongminhchau01032002@gmail.com>
2023-01-07 18:37:54 +0700

    add category section in sidebar

--- src/layouts/components/Sidebar/index.jsx
+++ src/layouts/components/Sidebar/index.jsx
@@ -1,5 +1,24 @@
 function Sidebar() {
-    return <aside className=" bg-pink-500">Sidebar</aside>;
+    return (
+        <aside className="">
+            {/* SECTION CATEGORY */}
+            <section className="mt-4">
+                <h2 className="text-xl font-semibold">Chủ đề</h2>
+                <div className="mt-2 flex flex-wrap">
+                    <button className="mr-2 mb-2 rounded-md bg-pink-400 px-5 py-1 text-white hover:bg-opacity-70">Lập trình</button>
+                    <button className="mr-2 mb-2 rounded-md bg-orange-500 px-5 py-1 text-white hover:bg-opacity-70">Bugs</button>
+                    <button className="mr-2 mb-2 rounded-md bg-violet-500 px-5 py-1 text-white hover:bg-opacity-70">Chia sẻ</button>
+                    <button className="mr-2 mb-2 rounded-md bg-green-600 px-5 py-1 text-white hover:bg-opacity-70">Cuộc sống</button>
+                </div>
+            </section>
+
+            {/* TOP POST SECTION */}
+            <section className="mt-4">
+                <h2 className="text-xl font-semibold">Nổi bật</h2>
+                <div className="mt-2 flex flex-wrap">àasffsaffasfdas</div>
+            </section>
+        </aside>
+    );
 }
 
 export default Sidebar;
