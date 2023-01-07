7b63cb8172f2b1ba42119f6844ee4330533602b5
Minh Chau <buitongminhchau01032002@gmail.com>
2023-01-07 18:47:30 +0700

    call api category in sidebar

--- src/layouts/components/Sidebar/index.jsx
+++ src/layouts/components/Sidebar/index.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
+
 function Sidebar() {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8080/api/categories')
+            .then((res) => res.json())
+            .then((resBody) => {
+                if (resBody.categories) {
+                    setCategories(resBody.categories);
+                } else {
+                    setCategories([]);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                setCategories([]);
+            });
+    }, []);
     return (
         <aside className="">
             {/* SECTION CATEGORY */}
             <section className="mt-4">
                 <h2 className="text-xl font-semibold">Chủ đề</h2>
                 <div className="mt-2 flex flex-wrap">
-                    <button className="mr-2 mb-2 rounded-md bg-pink-400 px-5 py-1 text-white hover:bg-opacity-70">Lập trình</button>
-                    <button className="mr-2 mb-2 rounded-md bg-orange-500 px-5 py-1 text-white hover:bg-opacity-70">Bugs</button>
-                    <button className="mr-2 mb-2 rounded-md bg-violet-500 px-5 py-1 text-white hover:bg-opacity-70">Chia sẻ</button>
-                    <button className="mr-2 mb-2 rounded-md bg-green-600 px-5 py-1 text-white hover:bg-opacity-70">Cuộc sống</button>
+                    {categories?.map((category) => (
+                        <button key={category._id} className="mr-2 mb-2 rounded-md bg-pink-400 px-5 py-1 text-white hover:bg-opacity-70">
+                            {category.name || 'Không tên'}
+                        </button>
+                    ))}
                 </div>
             </section>
 
