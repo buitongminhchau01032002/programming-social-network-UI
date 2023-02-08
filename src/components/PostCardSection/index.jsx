87dffa6165beff15705adc1694482fa6002c0b85
Đam Sa <20521154@ms.uit.edu.vn>
2023-02-08 14:21:32 +0700

    fix link post

--- src/components/PostCardSection/index.jsx
+++ src/components/PostCardSection/index.jsx
@@ -211,36 +211,39 @@ function PostCartSection({ postInit, postId, full }) {
 
     return (
         <div className=" mt-4 flex h-full  cursor-pointer flex-col justify-between rounded-lg border border-gray-300 p-3 px-3 py-2 text-left transition  hover:shadow-md ">
-            <div className="my-1 flex justify-between ">
-                <div className="flex">
-                    <UserWithAvatarAndName user={post?.creator} />
-                </div>
-                <div className="flex">
-                    <div className="mr-2 flex cursor-pointer select-none  items-center justify-center ">
-                        {moment(post?.createdAt).startOf('seconds').fromNow()}
+            <div onClick={() => navigate('/comment/' + post?._id)}>
+                <div className="my-1 flex justify-between ">
+                    <div className="flex" onClick={(e) => e.stopPropagation()}>
+                        <UserWithAvatarAndName user={post?.creator} />
                     </div>
-                    <button
-                        title=""
-                        className="mr-2 grid place-items-center items-center justify-center transition hover:text-slate-600"
-                    >
-                        <Save isSaved={isSaved} onToggle={handleToggleSave} />
-                    </button>
-                    {isOwner ? (
+                    <div className="flex">
+                        <div className="mr-2 flex cursor-pointer select-none  items-center justify-center ">
+                            {moment(post?.createdAt).startOf('seconds').fromNow()}
+                        </div>
                         <button
                             title=""
                             className="mr-2 grid place-items-center items-center justify-center transition hover:text-slate-600"
-                            onClick={() => {
-                                navigate('/edit-post/' + post._id);
-                            }}
+                            onClick={(e) => e.stopPropagation()}
                         >
-                            <i className=" fa-solid fa-pencil"></i>
+                            <Save isSaved={isSaved} onToggle={handleToggleSave} />
                         </button>
-                    ) : (
-                        <></>
-                    )}
+                        {isOwner ? (
+                            <button
+                                title=""
+                                className="mr-2 grid place-items-center items-center justify-center transition hover:text-slate-600"
+                                onClick={(e) => {
+                                    navigate('/edit-post/' + post._id);
+                                    e.stopPropagation();
+                                }}
+                            >
+                                <i className=" fa-solid fa-pencil"></i>
+                            </button>
+                        ) : (
+                            <></>
+                        )}
+                    </div>
                 </div>
-            </div>
-            <Link to={'/comment/' + post?._id}>
+
                 <h2
                     className={clsx('my-2  cursor-pointer select-none font-bold', {
                         'line-clamp-2 hover:line-clamp-none': full,
@@ -264,46 +267,48 @@ function PostCartSection({ postInit, postId, full }) {
                             <img src={image} className="h-full w-full object-cover" />
                         </div>
                     ))}
-            </Link>
 
-            <div className=" flex w-full items-center py-1   ">
-                <div className=" cursor-pointer select-none ">
-                    <span className="invisible absolute block select-none py-1 pr-3 sm:visible sm:static">
-                        <CategoryBadge category={post?.category}></CategoryBadge>
-                    </span>
-                    <path d="M4.5 12.75l6 6 9-13.5"></path>
-                </div>
+                <div className=" flex w-full items-center py-1   ">
+                    <div className=" cursor-pointer select-none ">
+                        <span className="invisible absolute block select-none py-1 pr-3 sm:visible sm:static">
+                            <CategoryBadge category={post?.category}></CategoryBadge>
+                        </span>
+                        <path d="M4.5 12.75l6 6 9-13.5"></path>
+                    </div>
 
-                <div className="cursor-pointer  select-none items-center  justify-center text-center align-middle text-xs text-slate-500  hover:underline">
-                    #{post?.tag?.name}
+                    <div className="cursor-pointer  select-none items-center  justify-center text-center align-middle text-xs text-slate-500  hover:underline">
+                        #{post?.tag?.name}
+                    </div>
                 </div>
-            </div>
-            <div className=" ml-2  flex w-full py-1">
-                <Like isLiked={isLiked} numberOfLike={post.likes?.length || 0} onToggle={handleToggleLike} />
-                <Link to={'/comment/' + post?._id}>
+                <div className=" ml-2  flex w-full py-1">
+                    <div onClick={(e) => e.stopPropagation()}>
+                        <Like isLiked={isLiked} numberOfLike={post.likes?.length || 0} onToggle={handleToggleLike} />
+                    </div>
+                    <Link to={'/comment/' + post?._id}>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="ml-2 h-5 w-5"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                                />
+                            </svg>
+                            <p className="ml-1">{post.numComments || 0}</p>
+                        </div>
+                    </Link>
                     <div className="flex items-center">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="ml-2 h-5 w-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-                            />
-                        </svg>
-                        <p className="ml-1">{post.numComments || 0}</p>
+                        <div>
+                            <i className="fa-regular fa-eye ml-2"></i>
+                        </div>
+                        <p className="ml-1">{post.views?.length || 0}</p>
                     </div>
-                </Link>
-                <div className="flex items-center">
-                    <div>
-                        <i className="fa-regular fa-eye ml-2"></i>
-                    </div>
-                    <p className="ml-1">{post.views?.length || 0}</p>
                 </div>
             </div>
         </div>
